9436c415959a27413a16f3fd438247fb0a035280
mattcriswell <matt.criswell@rackspace.com>
2014-11-23 11:27:00 -0600

    created defined type for vhosts

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,4 +1,11 @@
-#include nebtool
-class { 'nebtool':
-  tool_name => "little-dipper",
+include nebtool
+
+toolvhost {'little-dipper':
+ tool_name => "little-dipper",
+}
+toolvhost {'wyld-stallions':
+ tool_name => "wyld-stallions",
+}
+toolvhost {'hamburger-helper':
+ tool_name => "hamburger-helper",
 }
--- puppet/modules/nebtool/manifests/init.pp
+++ puppet/modules/nebtool/manifests/init.pp
@@ -1,52 +1,56 @@
 class nebtool ($tool_name = "default") {
 
-include apache
-#$tool_name = "little-dipper"
+  include apache
+  #$tool_name = "little-dipper"
 
-exec { 'iptables-flush': command => "/usr/sbin/iptables -F" }
-exec { 'disable-selinux': command => "/sbin/setenforce 0" }
+  exec { 'iptables-flush': command => "/usr/sbin/iptables -F" }
+  exec { 'disable-selinux': command => "/sbin/setenforce 0" }
 
-service { 'iptables':
-  require => exec['iptables-flush'],
-  ensure => stopped,
-}
+  service { 'iptables':
+    require => exec['iptables-flush'],
+    ensure => stopped,
+  }
 
-exec { 'yum-update': command => "/bin/yum update -y" }
+  exec { 'yum-update': command => "/bin/yum update -y" }
 
-file {'/var/www':
-  ensure => directory,
-  mode => 0755,
-}
+  file {'/var/www':
+    ensure => directory,
+    mode => 0755,
+  }
 
-file {'/var/www/pythonapp':
-  require => file['/var/www'],
-  ensure => directory,
-  mode => 0777,
-}
+  file {'/var/www/pythonapp':
+    require => file['/var/www'],
+    ensure => directory,
+    mode => 0777,
+  }
 
-vcsrepo { "/var/www/pythonapp/${tool_name}":
-  require => [package['git'],file['/var/www/pythonapp']],
-  ensure => present,
-  provider => git,
-  source => "https://github.com/mattcriswell/${tool_name}.git"
-}
+  package { "epel-release.noarch":
+    ensure => present,
+    require => exec['yum-update'],
+  }
 
-package { "epel-release.noarch":
-  ensure => present,
-  require => exec['yum-update'],
-}
+  package { "python-flask.noarch":
+    ensure => present,
+    require => package['epel-release.noarch'],
+  }
 
-package { "python-flask.noarch":
-  ensure => present,
-  require => package['epel-release.noarch'],
-}
+  package { "git":
+    ensure => present,
+  }
 
-package { "git":
-  ensure => present,
 }
 
 
-apache::vhost { "${tool_name}.example.com":
+define toolvhost ($tool_name) {
+
+  vcsrepo { "/var/www/pythonapp/${tool_name}":
+    require => [package['git'],file['/var/www/pythonapp']],
+    ensure => present,
+    provider => git,
+    source => "https://github.com/mattcriswell/${tool_name}.git"
+  }
+
+  apache::vhost { "${tool_name}.example.com":
     require		      => [vcsrepo["/var/www/pythonapp/${tool_name}"], file['/var/www/pythonapp'], package['python-flask.noarch']],
     port                        => '80',
     docroot                     => '/var/www/pythonapp',
@@ -56,7 +60,7 @@ apache::vhost { "${tool_name}.example.com":
       processes    => '2',
       threads      => '15',
       display-name => '%{GROUP}',
-     },
+    },
     wsgi_import_script          => "/var/www/pythonapp/${tool_name}/demo.wsgi",
     wsgi_import_script_options  =>
       { process-group => "${tool_name}", application-group => '%{GLOBAL}' },
